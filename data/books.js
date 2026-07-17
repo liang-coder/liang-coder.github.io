231038af68f05bc0fe36f0d3f89cc9a8f128d2c0
xl <116136627+liang-coder@users.noreply.github.com>
2026-07-17 17:19:53 +0800

    Add files via upload

--- data/books.js
+++ data/books.js
@@ -4,9 +4,9 @@ window.BOOKS_DATA = [
     "title": "山海经异闻录",
     "author": "佚名",
     "chapters": [
-      { "title": "第一章 初入山海", "file": "novels/sample_book/001.js" },
-      { "title": "第二章 异兽出没", "file": "novels/sample_book/002.js" },
-      { "title": "第三章 神秘遗迹", "file": "novels/sample_book/003.js" }
+      { "title": "第一章 初入山海", "file": "novels/sample_book/001.txt" },
+      { "title": "第二章 异兽出没", "file": "novels/sample_book/002.txt" },
+      { "title": "第三章 神秘遗迹", "file": "novels/sample_book/003.txt" }
     ]
   }
 ];
