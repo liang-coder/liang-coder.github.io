45dc4d105510624a9b1a8f8ba84a20b48de61537
xl <116136627+liang-coder@users.noreply.github.com>
2026-07-17 17:07:03 +0800

    Add files via upload

---
+++ data/books.js
@@ -0,0 +1,12 @@
+window.BOOKS_DATA = [
+  {
+    "id": "sample_book",
+    "title": "山海经异闻录",
+    "author": "佚名",
+    "chapters": [
+      { "title": "第一章 初入山海", "file": "novels/sample_book/001.js" },
+      { "title": "第二章 异兽出没", "file": "novels/sample_book/002.js" },
+      { "title": "第三章 神秘遗迹", "file": "novels/sample_book/003.js" }
+    ]
+  }
+];
